346c4f1466c71991307137b325224c1762cc125d
SergejShk <sshkurupij11@gmail.com>
2022-08-18 21:12:28 +0300

    change https

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const saveToken = {
   set(token) {
